51ba03626679d13ba5e9d2636c656cc990cd4fad
Karen Gasparyan <mrkaren92@gmail.com>
2022-03-30 21:44:35 +0400

    Initial commit

---
+++ src/main/java/com/example/myitemsrest/MyitemsRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.myitemsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyitemsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyitemsRestApplication.class, args);
+	}
+
+}
